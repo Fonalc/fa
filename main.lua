f6edaf0cfa06bd72b60cb685713a57233b4e47db
Fonalc <124433429+Fonalc@users.noreply.github.com>
2023-08-05 12:02:17 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,6 @@
+if workspace.Terrain:FindFirstChild("_Game").Admin.Pads:FindFirstChild(game.Players.LocalPlayer.Name.."'s admin") then
+	game.Players:Chat("pm me Loaded Fonalc's Test Admin")
+end
 local banned = {}
 function findPlayer(plr)
 	for _, a in pairs(game.Players:GetPlayers()) do
@@ -11,9 +14,13 @@ end
 spawn(function()
 	while wait() do
 		for _, plr in pairs(game.Players:GetPlayers()) do
-			if plr.Backpack:FindFirstChild("VampireVanquisher") then
+			if plr.Backpack:FindFirstChild("VampireVanquisher") or plr.Character:FindFirstChild("VampireVanquisher") then
 				game.Players:Chat("ungear "..plr.Name)
-				game.Players:Chat("h \n\n\n\n\n\n\n\n\n\n\n\n"..plr.DisplayName.." that VampireVanquisher makes u sus.")
+				game.Players:Chat("h \n\n\n\n\n\n\n\n\n\n\n\n"..plr.DisplayName.." that VampireVanquisher aint allowed around here.")
+			end
+			if table.find(banned, plr) then
+				game.Players:Chat("blind "..plr.Name)
+				game.Players:Chat("punish "..plr.Name)
 			end
 		end
 	end
@@ -21,19 +28,31 @@ end)
 
 game.Players.LocalPlayer.Chatted:Connect(function(msg)
 	local split = string.split(msg, ".")
-	if split[1] == "<spun>" then
-		local plr = game.Players:FindFirstChild(split[2])
-		if plr then
-			game.Players:Chat("pm "..plr.Name.." youve been trolled!!!\ni can't turn this off btw so ur stuck unless ur smart.")
-			while wait() do
-				game.Players:Chat("punish "..plr.Name)
-				game.Players:Chat("blind "..plr.Name)
+	if split[1] == "<spun" then
+		if split[2] == "all" then
+			for _, plr in pairs(game.Players:GetPlayers()) do
+				if plr then
+					game.Players:Chat("pm "..plr.Name.." ur banned lol.")
+					table.insert(banned, plr)
+				end
+			end
+		elseif split[2] == "me" then
+			table.insert(banned, game.Players.LocalPlayer)
+		else
+			local plr = game.Players:FindFirstChild(split[2])
+			if plr then
+				game.Players:Chat("pm "..plr.Name.." ur banned.")
+				table.insert(banned, plr)
 			end
 		end
 	end
-	if split[1] == "<tp pad>" then
-		if workspace.Terrain["_Game"].Admin.Pads:FindFirstChild("Touch to get admin") then
-			game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.Terrain["_Game"].Admin.Pads:FindFirstChild("Touch to get admin").Head.CFrame
+	if split[1] == "<sspun" then
+		local plr = game.Players:FindFirstChild(split[2])
+		if plr then
+			table.remove(banned, table.find(banned,plr))
+			game.Players:Chat("pm "..plr.Name.." ur unbanned.")
+			game.Players:Chat("unblind "..plr.Name)
+			game.Players:Chat("unpunish "..plr.Name)
 		end
 	end
 end)
