f7c49dc7c763451cb46eb47a37c68738ae5ff7b9
Fonalc <124433429+Fonalc@users.noreply.github.com>
2023-08-06 13:04:15 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -122,9 +122,9 @@ game.Players.LocalPlayer.Chatted:Connect(function(msg)
 		local plr = game.Players:FindFirstChild(split[2])
 		if plr then
 			spawn(function()
+				game.Players:Chat("jail "..plr.Name)
 				repeat
 					game.Players:Chat("skydive "..plr.Name)
-					game.Players:Chat("jail "..plr.Name)
 					game.Players:Chat("ff "..plr.Name)
 					wait(0)
 				until not plr.Character
