1e019d2627817705ab6535ee30e9178a6b3b8926
Fonalc <124433429+Fonalc@users.noreply.github.com>
2023-08-07 16:33:56 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 --<____________________________________________FA____________________________________________>--
-game.Players:Chat("h \n\n\n\n\n\n\n\n\n\n\n\nSuccessfully Loaded FA.\nEnjoy! \n(Say <cmds> or <cmdPrint>)")
+game.Players:Chat("h \n\n\n\n\n\n\n\n\n\n\n\nSuccessfully Loaded FA.\n(Say <cmds> or <cmdPrint>)")
 local banned = {}
 local sl = false
 local antideath = false
