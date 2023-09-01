5b14d73182ac34912b08279d2f70488414698cca
Fonalc <124433429+Fonalc@users.noreply.github.com>
2023-09-01 23:11:14 +0100

    fogeda

---
+++ Scripts/fog_dancer.lua
@@ -0,0 +1,16 @@
+function chat(text)
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(text, "All")
+end
+chat("Waiting for music to play...")
+repeat
+	wait()
+until workspace.Terrain._Game.Folder:FindFirstChild("Sound")
+chat("Music playing!")
+while workspace.Terrain._Game.Folder:FindFirstChild("Sound") do
+	game.Players:Chat("fogend "..workspace.Terrain._Game.Folder:FindFirstChild("Sound").PlaybackLoudness)
+	game.Players:Chat("fogcolor 255 0 0")
+	game.Players:Chat("time 0")
+	wait()
+end
+chat("Music ended.")
+game.Players:Chat("fix")
