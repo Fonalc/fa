58c3d1c52aeb79ccbc27c10d8f4bebece3504e70
Fonalc <124433429+Fonalc@users.noreply.github.com>
2023-09-02 17:53:50 +0100

    hft

--- Scripts/antis.lua
+++ Scripts/antis.lua
@@ -8,19 +8,19 @@ RunService.Stepped:Connect(function()
 end) 
 
 RunService.Stepped:Connect(function()
-    if Player.Character.Parent = game.Lighting then
+    if game.Lighting:FindFirstChild(Player.Character.Name) then
         game.Players:Chat("unpunish me")
     end
 end) 
 
 RunService.Stepped:Connect(function()
-    if game.Players.LocalPlayer.PlayerGui:FindFirstChild("EFFECTGUIBLIND") then
+    if Player.PlayerGui:FindFirstChild("EFFECTGUIBLIND") then
         game.Players:Chat("unblind me")
     end
 end) 
 
 RunService.Stepped:Connect(function()
-    if workspace.Terrain._Game.Folder:FindFirstChild(game.Players.LocalPlayer.Name.."'s jail") then
+    if workspace.Terrain._Game.Folder:FindFirstChild(Player.Name.."'s jail") then
         game.Players:Chat("unjail me")
     end
 end) 
