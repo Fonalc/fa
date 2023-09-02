74f2531319c6185ba9d421ed60d59c64156a4749
Fonalc <124433429+Fonalc@users.noreply.github.com>
2023-09-02 19:41:57 +0100

    MoreAntis

--- Scripts/antis.lua
+++ Scripts/antis.lua
@@ -24,3 +24,22 @@ RunService.Stepped:Connect(function()
         game.Players:Chat("unjail me")
     end
 end) 
+
+RunService.Stepped:Connect(function()
+    if workspace.Terrain._Game.Folder:FindFirstChild(Player.Name) then
+        game.Players:Chat("unclone me")
+    end
+end) 
+
+RunService.Stepped:Connect(function()
+    if game.Players.LocalPlayer.Character:FindFirstChild("Seizure") then
+        game.Players:Chat("unseizure me")
+    end
+end) 
+
+RunService.Stepped:Connect(function()
+    if not game.Players.LocalPlayer.Character:FindFirstChild("Seizure") and game.Players.LocalPlayer.Character.Humanoid:GetState().Name=="PlatformStanding" then
+        game.Players:Chat("unfly me")
+        game.Players:Chat("clip me")
+    end
+end) 
